ed84af0897a42f375d5b41d72e794dffe86bc4d2
Jordan Smith <smitj306@ferris.edu>
2022-01-13 16:25:30 -0500

    Start of Lerp Demo in Unity
    Started using Unity and created the script for the Lerp function

---
+++ UnityProject/Assets/Scripts/LerpDemo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpDemo : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
